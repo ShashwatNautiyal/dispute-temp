cea8dada63e8af2ffbdf06a67c36aa344943eb9a
Mikkel RINGAUD <mikkel@milescode.dev>
2023-01-23 13:56:26 +0100

    fix(Header): add ProviderIcon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import type { Component } from "solid-js";
+import { ProviderIconLarge } from "./ProviderContainer";
 
 const Header: Component<{
   title: string,
@@ -7,17 +8,20 @@ const Header: Component<{
   onActionPress: () => unknown,
   actionName: string
 }> = (props) => (
-  <div class="flex flex-col gap-2">
-    <div class="flex flex-col gap-[2px]">
-      <h1 class="text-[19px] leading-6 font-semibold">{props.title}</h1>
-      <p class="text-[15px] leading-5 font-normal text-[#86868B]">{props.description}</p>
-    </div>
-    <div>
-      <button class="text-[13px] leading-5 font-medium py-1 px-2 rounded-[16px] border border-blue-400 text-blue-400"
-        onClick={props.onActionPress}
-      >
-        {props.actionName}
-      </button>
+  <div class="flex gap-2">
+    <ProviderIconLarge />
+    <div class="flex flex-col gap-2">
+      <div class="flex flex-col gap-[2px]">
+        <h1 class="text-[19px] leading-6 font-semibold">{props.title}</h1>
+        <p class="text-[15px] leading-5 font-normal text-[#86868B]">{props.description}</p>
+      </div>
+      <div>
+        <button class="text-[13px] leading-5 font-medium py-1 px-2 rounded-[16px] border border-blue-400 text-blue-400"
+          onClick={props.onActionPress}
+        >
+          {props.actionName}
+        </button>
+      </div>
     </div>
   </div>
 );
