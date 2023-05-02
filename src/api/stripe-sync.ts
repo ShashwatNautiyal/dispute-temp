c16fa0d24966cba888b97fa50c3b4e9e44a294b5
harshmangalam <harshdev8218@gmail.com>
2023-05-02 15:39:30 +0530

    added disputes list

--- src/api/stripe-sync.ts
+++ src/api/stripe-sync.ts
@@ -4,6 +4,7 @@ const STRIPE_API_KEY = import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY;
 const STRIPE_ENDPOINT = "https://stripesync.vish3794.workers.dev";
 const token = `Bearer eyJhbGciOiJIUzUxMiIsImtpZCI6ImRpc3B1dGUifQ.eyJleHAiOjE2ODU4NzM5MjksIm5iZiI6MTY4Mjg3MzkyOSwiaWF0IjoxNjgyODczOTI5LCJzdWIiOiJhbmRyZXcudmlzb2tpaEBnbWFpbC5jb20iLCJhbGlhcyI6InVzZXItdGVzdC05ZmVkMTk2NS02N2Q2LTQ0N2UtOWQwZS0xYmU2NzM2YTBiYjMifQ.UANSDwuoCg9sf3VED-PT7gD4Kw5VW1JjD9sDNrvh_veiNVdx6FYglc_WPLlmJqABimLyqHOXoiaMBRxK6KNGfA`;
 
+axios.defaults.baseURL = STRIPE_ENDPOINT;
 axios.defaults.headers.common.Authorization = token;
 
 async function getRegisterKey() {
@@ -22,57 +23,58 @@ async function chargeSuccessed(payload: any) {
 }
 
 async function createDispute({ address, email, name }: any) {
-  const resp = await axios.post(
-    `${STRIPE_ENDPOINT}/dispute_created/${STRIPE_API_KEY}`,
-    {
-      data: {
-        object: {
-          id: crypto.randomUUID(),
-          amount: 20,
-          currency: "USD",
-          evidence: {
-            access_activity_log: "",
-            billing_address: "",
-            cancellation_policy: "",
-            cancellation_policy_disclosure: "z",
-            cancellation_rebuttal: "",
-            customer_communication: "",
-            customer_email_address: email,
-            customer_name: name,
-            customer_purchase_ip: "",
-            customer_signature: "",
-            duplicate_charge_documentation: "",
-            duplicate_charge_explanation: "",
-            duplicate_charge_id: "",
-            product_description: "",
-            receipt: "",
-            refund_policy: "",
-            refund_policy_disclosure: "",
-            refund_refusal_explanation: "",
-            service_date: "",
-            service_documentation: "",
-            shipping_address: address,
-            shipping_carrier: "",
-            shipping_date: "",
-            shipping_documentation: "",
-            shipping_tracking_number: "",
-            uncategorized_file: "",
-            uncategorized_text: "",
-          },
-          evidence_details: {
-            due_by: 100,
-            has_evidence: true,
-            past_due: true,
-            submission_count: 100,
-          },
-          reason: "Some reason",
-          status: "Failed",
+  const resp = await axios.post(`/dispute_created/${STRIPE_API_KEY}`, {
+    data: {
+      object: {
+        id: crypto.randomUUID(),
+        amount: 20,
+        currency: "USD",
+        evidence: {
+          access_activity_log: "",
+          billing_address: "",
+          cancellation_policy: "",
+          cancellation_policy_disclosure: "z",
+          cancellation_rebuttal: "",
+          customer_communication: "",
+          customer_email_address: email,
+          customer_name: name,
+          customer_purchase_ip: "",
+          customer_signature: "",
+          duplicate_charge_documentation: "",
+          duplicate_charge_explanation: "",
+          duplicate_charge_id: "",
+          product_description: "",
+          receipt: "",
+          refund_policy: "",
+          refund_policy_disclosure: "",
+          refund_refusal_explanation: "",
+          service_date: "",
+          service_documentation: "",
+          shipping_address: address,
+          shipping_carrier: "",
+          shipping_date: "",
+          shipping_documentation: "",
+          shipping_tracking_number: "",
+          uncategorized_file: "",
+          uncategorized_text: "",
         },
+        evidence_details: {
+          due_by: 100,
+          has_evidence: true,
+          past_due: true,
+          submission_count: 100,
+        },
+        reason: "Some reason",
+        status: "Failed",
       },
-    }
-  );
+    },
+  });
   console.log(resp);
   return resp.data;
 }
 
-export { getRegisterKey, chargeSuccessed, createDispute };
+async function getDisputes() {
+  return axios.get(`/disputes/${STRIPE_API_KEY}`);
+}
+
+export { getRegisterKey, chargeSuccessed, createDispute, getDisputes };
--- src/pages/provider.tsx
+++ src/pages/provider.tsx
@@ -1,4 +1,4 @@
-import type { Component } from "solid-js";
+import { Component, onMount } from "solid-js";
 
 import { For, Show, createMemo, createSignal } from "solid-js";
 import { A, Outlet } from "@solidjs/router";
@@ -8,40 +8,69 @@ import TransOverview from "@/components/TransOverview";
 import Search from "@/components/Search";
 
 import { TransOverviewItems } from "@/constants/providers";
+import { getDisputes } from "@/api/stripe-sync";
 
 const ProviderPage: Component = () => {
+  const [disputes, setDisputes] = createSignal([]);
+  onMount(async () => {
+    try {
+      const resp = await getDisputes();
+      console.log(resp.data);
+      setDisputes(resp.data);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
   const [searchValue, setSearchValue] = createSignal("");
-  const filteredTransOverviewItems = createMemo(() => searchValue() ?
-    TransOverviewItems.filter(
-      item => item.fromName.toLowerCase().includes(searchValue().toLowerCase()) || item.fromDescription.toLowerCase().includes(searchValue().toLowerCase())
-    ) : TransOverviewItems
+  const filteredTransOverviewItems = createMemo(() =>
+    searchValue()
+      ? TransOverviewItems.filter(
+          (item) =>
+            item.fromName.toLowerCase().includes(searchValue().toLowerCase()) ||
+            item.fromDescription
+              .toLowerCase()
+              .includes(searchValue().toLowerCase())
+        )
+      : TransOverviewItems
   );
 
   return (
     <main class="flex w-full h-full">
       <div class="flex flex-col flex-shrink-0 h-full border-r border-r-[#EDEDEF]">
         <div class="p-2">
-          <Search placeholder="Search Transactions"
+          <Search
+            placeholder="Search Transactions"
             value={searchValue()}
             onInput={(e) => setSearchValue(e.currentTarget.value)}
-
             showSearchActions={true}
           />
         </div>
 
         <div class="flex flex-col gap-[2px] p-2 h-full overflow-y-hidden mr-[var(--scrollbar-width)] hover:overflow-y-scroll hover:mr-0">
-          <For each={filteredTransOverviewItems()} fallback={
-            <div class="w-[344px]">
-              <p class="text-[#EDEDEF] text-center">No item found</p>
-            </div>
-          }>
-            {(item, index) => (
+          <For
+            each={disputes()}
+            fallback={
+              <div class="w-[344px]">
+                <p class="text-[#EDEDEF] text-center">No item found</p>
+              </div>
+            }
+          >
+            {(item: any, index) => (
               <>
                 <A href={`./${index()}`}>
-                  <TransOverview {...item} />
+                  <TransOverview
+                    fromName={item.evidence.customer_name}
+                    cardAmount={item.amount}
+                    cardDigits="5567"
+                    fromDate={new Date()}
+                    fromDescription={item.reson}
+                  />
                 </A>
 
-                <Show when={index() !== filteredTransOverviewItems().length - 1}>
+                <Show
+                  when={index() !== filteredTransOverviewItems().length - 1}
+                >
                   <hr class="text-[#EDEDEF]" />
                 </Show>
               </>
